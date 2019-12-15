83f5acfeab77af293ec6ca72fe89b759a2d236b8
Tom Selfin <selfint@gmail.com>
2019-12-15 15:45:49 +0200

    Add categorical cross entropy cost function
    Just copied a formula, not sure if this is correct
    TODO: make sure this works correctly

--- ANN/cost_functions.py
+++ ANN/cost_functions.py
@@ -5,24 +5,24 @@ from typing import Union
 def mse(
     prediction: np.array, target: np.array, deriv: bool = False
 ) -> Union[np.array, float]:
-    """Mean squared error cost function
-    
-    Arguments:
-        prediction {np.array} -- network output
-        target {np.array} -- target network output
-    
-    Keyword Arguments:
-        deriv {bool} -- get the derivate of the cost function (default: {False})
-    
-    Returns:
-        Union[np.array, float] -- returns the derivate of the cost function
-        for each neuron in the output layer (np.array),
-        or the cost of the network as a whole (float)
-    """
     if deriv:
         return 2 * (prediction - target)
     else:
         return np.mean(pow(prediction - target, 2))
 
 
-COST_FUNCTIONS = {"mse": mse, "mean squared error": mse}
+def cce(
+    prediction: np.array, target: np.array, deriv: bool = False
+) -> Union[np.array, float]:
+    if deriv:
+        return -1 * (target / (prediction + 1e-15)) + (1 - target) / (1 - prediction + 1e-15)
+    return -1 * sum(target * np.log(prediction))
+
+
+COST_FUNCTIONS = {
+    "mse": mse,
+    "mean squared error": mse,
+    "cce": cce,
+    "catgorical cross entropy": cce,
+}
+
