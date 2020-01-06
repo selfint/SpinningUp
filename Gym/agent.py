b5af6e1553cb4560af3086435097b654ffafa167
Tom Selfin <selfint@gmail.com>
2020-01-06 08:48:29 +0200

    add action and observation space type
    the agents need to know if the action and observation spaces are
    continuous or discrete

--- Gym/agent.py
+++ Gym/agent.py
@@ -1,7 +1,11 @@
 class Agent:
-    def __init__(self, action_dim, observation_dim):
+    def __init__(
+        self, action_dim, observation_dim, action_space_type, observation_space_type
+    ):
         self.action_dim = action_dim
         self.observation_dim = observation_dim
+        self.action_space_type = action_space_type
+        self.observation_space_type = observation_space_type
 
     def act(self, observation):
         """return an action from the action space
