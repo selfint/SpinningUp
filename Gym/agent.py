45f4de24defb750abde64b709f82ec15f364e463
Tom Selfin <selfint@gmail.com>
2020-01-06 08:28:55 +0200

    implement abstract agent
    all algorithms will be agents that learn and act differently

---
+++ Gym/agent.py
@@ -0,0 +1,28 @@
+class Agent:
+
+    def __init__(self, action_dim, observation_dim):
+        self.action_dim = action_dim
+        self.observation_dim = observation_dim
+
+    def act(self, observation):
+        """return an action from the action space
+        
+        Arguments:
+            observation {observation} -- env observation
+        """
+        raise NotImplementedError()
+
+    def learn(self, observation, action, reward, new_observation):
+        """learn from action
+        
+        Arguments:
+            observation {observation} -- env observation
+            action {action} -- action agent took
+            reward {float} -- reward given after action in observation
+            new_observation {observation} -- env observation following agent action
+        """
+        raise NotImplementedError()
+
+
+
+
